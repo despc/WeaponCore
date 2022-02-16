0558def183c1ffdc6801cba9f497e4f9fb15dbcd
despc <idespc@gmail.com>
2022-02-17 00:09:02 +0300

    Damage to own grid from smart projectiles

--- Data/Scripts/CoreSystems/Session/SessionDamageMgr.cs
+++ Data/Scripts/CoreSystems/Session/SessionDamageMgr.cs
@@ -287,7 +287,7 @@ namespace CoreSystems
                 return;
             }
             
-            if (t.AmmoDef.DamageScales.Shields.Type == ShieldDef.ShieldType.Heal || (!t.AmmoDef.Const.SelfDamage && !t.AmmoDef.Const.IsCriticalReaction && !t.SmartReady) && t.Ai.AiType == Ai.AiTypes.Grid && t.Ai.GridEntity.IsInSameLogicalGroupAs(grid) || !grid.DestructibleBlocks || grid.Immune || grid.GridGeneralDamageModifier <= 0)
+            if (t.AmmoDef.DamageScales.Shields.Type == ShieldDef.ShieldType.Heal || (!t.AmmoDef.Const.SelfDamage && !t.AmmoDef.Const.IsCriticalReaction) && t.Ai.AiType == Ai.AiTypes.Grid && t.Ai.GridEntity.IsInSameLogicalGroupAs(grid) || !grid.DestructibleBlocks || grid.Immune || grid.GridGeneralDamageModifier <= 0)
             {
                 t.BaseDamagePool = 0;
                 return;
